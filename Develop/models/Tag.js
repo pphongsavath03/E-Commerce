61fed77b57ca2a7f47ff65bbe40d6a3a92eeb418
Phon Phongsavath <pphongsavath03@gmail.com>
2021-10-26 23:39:59 -0400

    updates

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -4,23 +4,26 @@ const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
 
-Tag.init({
+Tag.init(
+  {
     // define columns
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        allowNull: false,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true
     },
     tag_name: {
-        type: DataTypes.STRING
+      type: DataTypes.STRING
     }
-}, {
+  },
+  {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'tag',
-});
+  }
+);
 
 module.exports = Tag;
